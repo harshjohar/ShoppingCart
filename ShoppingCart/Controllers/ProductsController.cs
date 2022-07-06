4e43c9d4342cb2c10e89ccf4fd3ff3a101c7d598
Harshpreet Singh Johar <hpsj3112@gmail.com>
2022-07-07 04:33:00 +0530

    products display

---
+++ ShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Infrastructure;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public ProductsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IActionResult> Index(string categorySlug = "", int p = 1)
+        {
+            int pageSize = 3;
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.CategorySlug = categorySlug;
+
+            if (categorySlug == "")
+            {
+                ViewBag.TotalPages = (int)Math.Ceiling((decimal)_dataContext.Products.Count() / pageSize);
+                return View(await _dataContext.Products.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
+            }
+
+            Category category = await _dataContext.Categories.Where(c => c.Slug == categorySlug).FirstOrDefaultAsync();
+            if (category == null) return RedirectToAction("Index");
+
+            var productsByCategory = _dataContext.Products.Where(p=>p.CategoryId==category.Id);
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)productsByCategory.Count() / pageSize);
+
+            return View(await productsByCategory.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
+        }
+    }
+}
