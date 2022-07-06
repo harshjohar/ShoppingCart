9ef8ca772424ab3882fce19c3e844513dc4419d9
Harsh Johar <hpsj3112@gmail.com>
2022-07-07 03:19:49 +0530

    infra

---
+++ ShoppingCart/Infrastructure/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Category> Categories { get; set; }
+    }
+}
+
